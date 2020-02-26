9de64ad0c91c593ee65eb15910716d17aab925ca
victorcwyu <victorcwyu@gmail.com>
2020-02-26 19:31:19 +0000

    Implemented AJAX for posting and getting data to/from server

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,65 +1,60 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// A page can't be manipulated safely until the document is "ready." jQuery detects this state of readiness for you.
+// Code included inside $(document).ready() will only run once the page Document Object Model(DOM) is ready for JavaScript code to execute
 
-// Fake data taken from initial-tweets.json
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png"
-      ,
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+$(document).ready(function () {
+  
+  loadtweets();
+  const $form = $('#newbie');
+  $form.on('submit', function (event) {
+    event.preventDefault()
+    // Post serialized data to AJAX, to add new tweet
+    $.ajax({
+      url: '/tweets',
+      method: 'POST',
+      data: $form.serialize(),
+      // successful post renders updated list of tweets
+      success: function() {
+        loadtweets()
+      }
+    }
+    )
+  });
+});
 
+// deleted fake data taken from initial-tweets.json
 
-$(document).ready(function() {
+// makes a get request, returns a promise, uses that result to render the tweets, passing in the result
+const loadtweets = function (tweets) {
+    $.ajax('/tweets', { method: 'GET', dataType: 'JSON' })
+      .then(function(result) {renderTweets(result)});
+    }
 
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
-  const renderTweets = function(tweets) {
-    tweets.forEach(element => {
-      $('#tweet-container').append(createTweetElement(element));
-    });
-  };
+// loops through tweets
+// calls createTweetElement for each tweet
+// takes return value and appends it to the tweets container
+const renderTweets = function (tweets) {
+  tweets.forEach(element => {
+    //changed append to prepend => makes new tweets appear at top
+    $('#tweet-container').prepend(createTweetElement(element));
+  });
+};
 
-  // takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
-  const createTweetElement = (tweetData) => {
-    const $tweet = $("<article>").addClass("tweet");
-    
-    const total_seconds = parseInt(Math.floor(tweetData.created_at / 1000));
-    const total_minutes = parseInt(Math.floor(total_seconds / 60));
-    const total_hours = parseInt(Math.floor(total_minutes / 60));
-    const days = parseInt(Math.floor(total_hours / 24));
-    const years = parseInt(Math.floor(days / 365));
-    const hours = parseInt(total_hours % 24);
-    const minutes = parseInt(total_minutes % 60);
-    const seconds = parseInt(total_seconds % 60);
+// takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
+const createTweetElement = (tweetData) => {
+  const $tweet = $("<article>").addClass("tweet");
 
-    const date = `${years} years, ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds ago.`;
+  const total_seconds = parseInt(Math.floor(tweetData.created_at / 1000));
+  const total_minutes = parseInt(Math.floor(total_seconds / 60));
+  const total_hours = parseInt(Math.floor(total_minutes / 60));
+  const days = parseInt(Math.floor(total_hours / 24));
+  const years = parseInt(Math.floor(days / 365));
+  const hours = parseInt(total_hours % 24);
+  const minutes = parseInt(total_minutes % 60);
+  const seconds = parseInt(total_seconds % 60);
 
-    const html =
-    `
+  const date = `${years} years, ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds ago.`;
+
+  const html = `
     <header>
       <div class="profile">
         <img src="${tweetData.user.avatars}">
@@ -76,10 +71,10 @@ $(document).ready(function() {
         <i class="fas fa-heart"></i>
       </div>
     </footer>
-    `;
-    $tweet.append(html);
-    return $tweet;
-  };
+  `;
 
-  renderTweets(data);
-});
+  $tweet.append(html);
+  return $tweet;
+};
+
+//deleted renderTweets

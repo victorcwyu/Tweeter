aaee2bddd6c6933b5e386a8df1b84a92f63f73bd
victorcwyu <victorcwyu@gmail.com>
2020-02-26 02:50:54 +0000

    Commented and linted code, and made the header full bleed for fun!

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,8 +4,6 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-$(document).ready(function () {
-
 // Fake data taken from initial-tweets.json
 const data = [
   {
@@ -31,48 +29,57 @@ const data = [
     },
     "created_at": 1461113959088
   }
-]
+];
 
-const renderTweets = function (tweets) {
-  tweets.forEach(element => {
-    $('#tweet-container').append(createTweetElement(element));
-  })
-}
 
-const createTweetElement = (tweetData) => {
-  const $tweet = $("<article>").addClass("tweet");
-  const total_seconds = parseInt(Math.floor(tweetData.created_at / 1000));
-  const total_minutes = parseInt(Math.floor(total_seconds / 60));
-  const total_hours = parseInt(Math.floor(total_minutes / 60));
-  const days = parseInt(Math.floor(total_hours / 24));
-  const years = parseInt(Math.floor(days / 365));
-  const hours = parseInt(total_hours % 24);
-  const minutes = parseInt(total_minutes % 60);
-  const seconds = parseInt(total_seconds % 60);
+$(document).ready(function() {
 
-  const date = `${years} years, ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds ago.`
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
+  const renderTweets = function(tweets) {
+    tweets.forEach(element => {
+      $('#tweet-container').append(createTweetElement(element));
+    });
+  };
 
-  const html =
-    `       <header>
-              <div class="profile">
-                <img src="${tweetData.user.avatars}">
-                <p>${tweetData.user.name}</p>
-              </div>
-              <span class="handle">${tweetData.user.handle}</span>
-            </header>
-            <p>${tweetData.content.text}</p>
-            <footer>
-              <p>${date}</p>
-              <div class="linkage">
-                <i class="fas fa-flag"></i>
-                <i class="fas fa-retweet"></i>
-                <i class="fas fa-heart"></i>
-              </div>
-            </footer>
+  // takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.
+  const createTweetElement = (tweetData) => {
+    const $tweet = $("<article>").addClass("tweet");
+    
+    const total_seconds = parseInt(Math.floor(tweetData.created_at / 1000));
+    const total_minutes = parseInt(Math.floor(total_seconds / 60));
+    const total_hours = parseInt(Math.floor(total_minutes / 60));
+    const days = parseInt(Math.floor(total_hours / 24));
+    const years = parseInt(Math.floor(days / 365));
+    const hours = parseInt(total_hours % 24);
+    const minutes = parseInt(total_minutes % 60);
+    const seconds = parseInt(total_seconds % 60);
+
+    const date = `${years} years, ${days} days, ${hours} hours, ${minutes} minutes, ${seconds} seconds ago.`;
+
+    const html =
     `
-  $tweet.append(html);
-  return $tweet;
-}
+    <header>
+      <div class="profile">
+        <img src="${tweetData.user.avatars}">
+        <p>${tweetData.user.name}</p>
+      </div>
+      <span class="handle">${tweetData.user.handle}</span>
+    </header>
+    <p>${tweetData.content.text}</p>
+    <footer>
+      <p>${date}</p>
+      <div class="linkage">
+        <i class="fas fa-flag"></i>
+        <i class="fas fa-retweet"></i>
+        <i class="fas fa-heart"></i>
+      </div>
+    </footer>
+    `;
+    $tweet.append(html);
+    return $tweet;
+  };
 
-renderTweets(data);
-});  
+  renderTweets(data);
+});
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,8 @@
-$(document).ready(function () {
-  $("#twit").on('input', function () {
-    let count = $(this).val().length
-    $(this).parent().children('.counter').text(140 - count)
+// implements the character counter for the tweet compose form
+$(document).ready(function() {
+  $("#twit").on('input', function() {
+    let count = $(this).val().length;
+    $(this).parent().children('.counter').text(140 - count);
     if (count > 140) {
       $(this).parent().children('.counter').addClass("invalid");
     } else {
